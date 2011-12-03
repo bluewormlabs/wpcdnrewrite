884d5759c3e5de426a2a81242d64195e6de56425
Chris McMacken <chris.mcmacken@gmail.com>
2011-12-03 15:57:04 -0600

    Working on settings page

--- html/config.php
+++ html/config.php
@@ -1,6 +1,10 @@
 <?php require_once('header.inc.php'); ?>
 
 <form id="wpcdnrewrite-config-form" method="post" action="<?php echo wp_nonce_url('options-general.php?page=' . WP_CDN_Rewrite::SLUG); ?>">
+    <?php
+        settings_fields('wpcdnrewrite');
+        do_settings_fields('wpcdnrewrite');
+    ?>
 	<a class="button-secondary" href="options-general.php?page=<?php echo WP_CDN_Rewrite::SLUG; ?>">Cancel</a> 
 	<input class="button-primary" type="submit" Name="save" value="<?php _e('Save Options'); ?>" id="submitbutton" />
 </form>
