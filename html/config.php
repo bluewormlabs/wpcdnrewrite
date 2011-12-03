e4234a2f9c0d2ba4a9cf6450d93643a2ed01f086
Ross Nelson <ross.nelson@gmail.com>
2011-12-03 13:30:10 -0600

    Fixed uses of ::SLUG from outside the class

--- html/config.php
+++ html/config.php
@@ -1,7 +1,7 @@
 <?php require_once('header.inc.php'); ?>
 
-<form id="wpcdnrewrite-config-form" method="post" action="<?php echo wp_nonce_url('options-general.php?page=' . self::$slug); ?>">
-	<a class="button-secondary" href="options-general.php?page=<?php echo self::$slug; ?>">Cancel</a> 
+<form id="wpcdnrewrite-config-form" method="post" action="<?php echo wp_nonce_url('options-general.php?page=' . WP_CDN_Rewrite::SLUG); ?>">
+	<a class="button-secondary" href="options-general.php?page=<?php echo WP_CDN_Rewrite::SLUG; ?>">Cancel</a> 
 	<input class="button-primary" type="submit" Name="save" value="<?php _e('Save Options'); ?>" id="submitbutton" />
 </form>
 
