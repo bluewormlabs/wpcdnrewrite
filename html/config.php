19d0a189105bf0db22e9f6fba9fd94bda79eebc8
Chris McMacken <chris.mcmacken@gmail.com>
2011-12-03 22:49:00 -0600

    Working on implementing the whitelist

--- html/config.php
+++ html/config.php
@@ -1,12 +1,58 @@
 <?php require_once('header.inc.php'); ?>
 
 <form id="wpcdnrewrite-config-form" method="post" action="<?php echo wp_nonce_url('options-general.php?page=' . WP_CDN_Rewrite::SLUG); ?>">
-    <?php
-        settings_fields('wpcdnrewrite');
-        do_settings_fields('wpcdnrewrite');
-    ?>
-	<a class="button-secondary" href="options-general.php?page=<?php echo WP_CDN_Rewrite::SLUG; ?>">Cancel</a> 
-	<input class="button-primary" type="submit" Name="save" value="<?php _e('Save Options'); ?>" id="submitbutton" />
+    <table id="whitelistTable">
+        <tr>
+            <td colspan="2"><h3>Domains to do rewriting for</h3></td>
+        </tr>
+<?php
+        $domainArray = get_option(WP_CDN_Rewrite::WHITELIST_KEY);
+
+        $count = 0;
+        if(count($domainArray) > 0) {
+            foreach($domainArray as $domain) {
+?>
+                <tr style="padding: 5px;" id="domain<?php echo $count;?>">
+                    <td>
+                        <input id="whitelist<?php echo $count;?>" name="<?php echo WP_CDN_Rewrite::WHITELIST_KEY;?>[]" type="text" value="<?php echo $domain ?>"/>
+                    </td>
+<?php
+                        if($count !== 0) {
+?>
+                            <td>
+                                <input type="button" class="button-primary" id="removeDomain<?php echo $count;?>" value="-" />
+                            </td>
+                    <?php
+                        }
+?>
+                </tr>
+<?php
+                $count++;
+            }
+?>
+                <tr id="addRow">
+                    <td colspan="2">
+
+                    </td>
+                    <td>
+                        <input type="button" class="button-primary" id="addDomain" value="Add" />
+                    </td>
+                </tr>
+<?php
+        }
+?>
+    </table>
+    <table>
+        <tr>
+            <td>
+                <a class="button-secondary" href="options-general.php?page=<?php echo WP_CDN_Rewrite::SLUG; ?>">Cancel</a>
+            </td>
+            <td>
+                <input class="button-primary" type="submit" Name="save" value="<?php _e('Save Options'); ?>" id="submitbutton" />
+                <?php settings_fields('wpcdnrewrite'); ?>
+            </td>
+        </tr>        
+    </table>
 </form>
 
 <?php require_once('footer.inc.php'); ?>
